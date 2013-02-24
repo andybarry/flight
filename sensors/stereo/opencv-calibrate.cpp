85a07bd812a8352fa5e37f90bace97ea2e25db72
Andy Barry <abarry@gmail.com>
2013-02-24 18:22:25 +0000

    Adds external (http://blog.martinperis.com/2011/01/opencv-stereo-camera-calibration.html) calibration library and my scripts/code to integrate with it.  That library has been modified to fit my setup (for wide angle lenses and pre-computed intrinsics)

---
+++ sensors/stereo/opencv-calibrate.cpp
@@ -0,0 +1,278 @@
+/*
+ * Use Gtk+ to show live video from the dc1394 camera
+ *
+ * Written by John Stowers <john.stowers@gmail.com>
+ * Modified for STEREO VISION PROCESSING BY ANDREW BARRY <abarry@csail.mit.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+ 
+#include <cv.h>
+#include <highgui.h>
+#include "opencv2/legacy/legacy.hpp"
+#include <sys/time.h>
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+extern "C"
+{
+    #include <stdio.h>
+    #include <stdint.h>
+    #include <stdlib.h>
+    #include <inttypes.h>
+
+    #include <glib.h>
+    #include <dc1394/dc1394.h>
+
+    #include "camera.h"
+    #include "utils.h"
+    #include "opencvutils.h"
+}
+
+#define BRIGHTNESS_VALUE 78
+#define EXPOSURE_VALUE 128
+
+#define MAX_FRAMES 2000
+
+#define CHESS_X 9
+#define CHESS_Y 6
+
+
+int camera_custom_setup(dc1394camera_t *camera);
+
+int main(int argc, char *argv[])
+{
+    dc1394_t        *d;
+    dc1394camera_t  *camera;
+    IplImage        *frame;
+    dc1394error_t   err;
+    guint64         guid = 0x00b09d0100af04d8;
+    
+    IplImage *frameArray[MAX_FRAMES];
+    IplImage *frameArray2[MAX_FRAMES];
+    
+    dc1394video_frame_t *frame_raw1;
+    dc1394video_frame_t *frame_raw2;
+    
+    unsigned long elapsed;
+    int numFrames = 0;
+    
+    // ----- cam 2 -----
+    dc1394_t        *d2;
+    dc1394camera_t  *camera2;
+    IplImage        *frame2;
+    dc1394error_t   err2;
+    guint64         guid2 = 0x00b09d0100a01ac5;
+
+    d = dc1394_new ();
+    if (!d)
+        g_critical("Could not create dc1394 context");
+        
+    d2 = dc1394_new ();
+    if (!d2)
+        g_critical("Could not create dc1394 context for camera 2");
+
+    camera = dc1394_camera_new (d, guid);
+    if (!camera)
+        g_critical("Could not create dc1394 camera");
+
+    camera2 = dc1394_camera_new (d2, guid2);
+    if (!camera2)
+        g_critical("Could not create dc1394 camera for camera 2");
+
+    // setup
+    err = setup_gray_capture(camera, DC1394_VIDEO_MODE_FORMAT7_1);
+    DC1394_ERR_CLN_RTN(err, cleanup_and_exit(camera), "Could not setup camera");
+
+    err2 = setup_gray_capture(camera2, DC1394_VIDEO_MODE_FORMAT7_1);
+    DC1394_ERR_CLN_RTN(err2, cleanup_and_exit(camera2), "Could not setup camera number 2");
+    
+    // enable auto-exposure
+    // turn on the auto exposure feature
+    err = dc1394_feature_set_power(camera, DC1394_FEATURE_EXPOSURE, DC1394_ON);
+    DC1394_ERR_RTN(err,"Could not turn on the exposure feature");
+    
+    err = dc1394_feature_set_mode(camera, DC1394_FEATURE_EXPOSURE, DC1394_FEATURE_MODE_ONE_PUSH_AUTO);
+    DC1394_ERR_RTN(err,"Could not turn on Auto-exposure");
+    
+    // enable auto-exposure
+    // turn on the auto exposure feature
+    err = dc1394_feature_set_power(camera2, DC1394_FEATURE_EXPOSURE, DC1394_ON);
+    DC1394_ERR_RTN(err,"Could not turn on the exposure feature for cam2");
+    
+    err = dc1394_feature_set_mode(camera2, DC1394_FEATURE_EXPOSURE, DC1394_FEATURE_MODE_ONE_PUSH_AUTO);
+    DC1394_ERR_RTN(err,"Could not turn on Auto-exposure for cam2");
+    
+    // enable camera
+    err = dc1394_video_set_transmission(camera, DC1394_ON);
+    DC1394_ERR_CLN_RTN(err, cleanup_and_exit(camera), "Could not start camera iso transmission");
+    err2 = dc1394_video_set_transmission(camera2, DC1394_ON);
+    DC1394_ERR_CLN_RTN(err2, cleanup_and_exit(camera2), "Could not start camera iso transmission for camera number 2");
+
+	cvNamedWindow("Input", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Input2", CV_WINDOW_AUTOSIZE);
+	
+    CvVideoWriter *writer = NULL;
+    CvVideoWriter *writer2 = NULL;
+    
+    CvSize size;
+    
+    Mat corners, cornersR;
+    
+    int i;
+    while (numFrames < MAX_FRAMES) {
+        // each loop dump a bunch of frames to clear the buffer
+        for (i=0;i<10;i++)
+        {
+            frame = dc1394_capture_get_iplimage(camera);
+            frame2 = dc1394_capture_get_iplimage(camera2);
+        }
+        
+        IplImage *image_left = frame;
+        IplImage *image_right = frame2;
+        // image_left and image_right are the input 8-bit
+        // single-channel images
+        // from the left and the right cameras, respectively
+        
+         size = cvGetSize(image_left);
+        
+        
+        // copy the images for drawing/display
+        Mat chessL = Mat(image_left, true);
+        Mat chessR = Mat(image_right, true);
+        Mat chessLc;
+        chessLc.create(size, CV_32FC3);
+        Mat chessRc;
+        chessRc.create(size, CV_32FC3);
+
+        // attempt checkerboard matching
+        bool foundPattern = findChessboardCorners((Mat)image_left, Size(CHESS_X, CHESS_Y), corners);
+        
+        foundPattern = foundPattern & findChessboardCorners((Mat)image_right, Size(CHESS_X, CHESS_Y), cornersR);
+        
+        
+        cvtColor( chessL, chessLc, CV_GRAY2BGR );
+        cvtColor( chessR, chessRc, CV_GRAY2BGR );
+        drawChessboardCorners(chessLc, Size(CHESS_X, CHESS_Y), corners, foundPattern);
+        
+        drawChessboardCorners(chessRc, Size(CHESS_X, CHESS_Y), cornersR, foundPattern);
+
+        imshow("Input", chessLc);
+	    imshow("Input2", chessRc);
+		
+		// key codes:
+		// page up: 654365
+		// page down: 65366
+		// b: 98
+		
+		char key = waitKey();
+		//printf("%d\n", (int)key);
+		if (key == 98)
+		{
+		    break;
+		} else if (key == 86){
+		    // this was a good one -- save it
+		    frameArray[numFrames] = image_left;
+            frameArray2[numFrames] = image_right;
+
+            printf("Saved frame %d\n", numFrames);
+            
+            numFrames ++;
+		}
+		
+	}
+
+    printf("\n\n");
+    
+    writer = cvCreateVideoWriter("testvid.avi", CV_FOURCC('P','I','M','1'), 30, size, 0 );
+    writer2 = cvCreateVideoWriter("testvid2.avi", CV_FOURCC('P','I','M','1'), 30, size, 0 );
+    
+    for (i=0;i<numFrames;i++)
+    {
+        printf("hi\n");
+        cvWriteFrame(writer, frameArray[i]);
+        cvWriteFrame(writer2, frameArray2[i]);
+        printf("Writing frame %d", i);
+    }
+    
+    printf("\n\n");
+    
+    cvDestroyWindow("Input");
+    cvDestroyWindow("Input2");
+
+    // stop data transmission
+    err = dc1394_video_set_transmission(camera, DC1394_OFF);
+    DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not stop the camera");
+    
+    err2 = dc1394_video_set_transmission(camera2, DC1394_OFF);
+    DC1394_ERR_CLN_RTN(err2,cleanup_and_exit(camera2),"Could not stop the camera 2");
+
+    // close camera
+    cleanup_and_exit(camera);
+    cleanup_and_exit(camera2);
+    dc1394_free (d);
+    dc1394_free (d2);
+    
+    cvReleaseVideoWriter(&writer);
+    cvReleaseVideoWriter(&writer2);
+
+    return 0;
+}
+
+int camera_custom_setup(dc1394camera_t *camera)
+{
+    uint32_t min, max;
+    dc1394error_t   err;
+    
+    /* turn off auto exposure */
+    /* turn on the feature - dont know what this means?? */
+    err = dc1394_feature_set_power(camera, DC1394_FEATURE_EXPOSURE, DC1394_ON);
+    DC1394_ERR_RTN(err,"Could not turn on the exposure feature");
+    
+    err = dc1394_feature_set_mode(camera, DC1394_FEATURE_EXPOSURE, DC1394_FEATURE_MODE_MANUAL);
+    DC1394_ERR_RTN(err,"Could not turn off Auto-exposure");
+    
+    //err = dc1394_feature_set_value(camera, DC1394_FEATURE_GAMMA, 1);
+    //DC1394_ERR_RTN(err, "Count not set gamma.");
+
+    /* get bounds and set */
+    err = dc1394_feature_get_boundaries(camera, DC1394_FEATURE_EXPOSURE, &min, &max);
+    DC1394_ERR_RTN(err,"Could not get bounds");
+
+    err = dc1394_feature_set_value(camera, DC1394_FEATURE_EXPOSURE, CLAMP(EXPOSURE_VALUE, min, max));
+    DC1394_ERR_RTN(err,"Could not set value");
+    
+    // disable auto-brightness since it is causing issues
+    /* turn on the feature - dont know what this means?? */
+    err = dc1394_feature_set_power(camera, DC1394_FEATURE_BRIGHTNESS, DC1394_ON);
+    DC1394_ERR_RTN(err,"Could not turn on the brightness feature");
+    
+    /* turn off auto exposure */
+    err = dc1394_feature_set_mode(camera, DC1394_FEATURE_BRIGHTNESS, DC1394_FEATURE_MODE_MANUAL);
+    DC1394_ERR_RTN(err,"Could not turn off Auto-brightness");
+
+    /* get bounds and set */
+    err = dc1394_feature_get_boundaries(camera, DC1394_FEATURE_BRIGHTNESS, &min, &max);
+    DC1394_ERR_RTN(err,"Could not get bounds");
+
+    err = dc1394_feature_set_value(camera, DC1394_FEATURE_BRIGHTNESS, CLAMP(BRIGHTNESS_VALUE, min, max));
+    DC1394_ERR_RTN(err,"Could not set value");
+    
+    // --- end brightness --- //
+}
+
---
+++ sensors/stereo/stereo_camera_calibrate/stereo_calibrate.cpp
@@ -0,0 +1,522 @@
+#pragma warning( disable: 4996 )
+/* *************** License:**************************
+   Oct. 3, 2008
+   Right to use this code in any way you want without warrenty, support or any guarentee of it working.
+
+   BOOK: It would be nice if you cited it:
+   Learning OpenCV: Computer Vision with the OpenCV Library
+     by Gary Bradski and Adrian Kaehler
+     Published by O'Reilly Media, October 3, 2008
+ 
+   AVAILABLE AT: 
+     http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
+     Or: http://oreilly.com/catalog/9780596516130/
+     ISBN-10: 0596516134 or: ISBN-13: 978-0596516130    
+
+   OTHER OPENCV SITES:
+   * The source code is on sourceforge at:
+     http://sourceforge.net/projects/opencvlibrary/
+   * The OpenCV wiki page (As of Oct 1, 2008 this is down for changing over servers, but should come back):
+     http://opencvlibrary.sourceforge.net/
+   * An active user group is at:
+     http://tech.groups.yahoo.com/group/OpenCV/
+   * The minutes of weekly OpenCV development meetings are at:
+     http://pr.willowgarage.com/wiki/OpenCV
+   ************************************************** */
+
+/*
+	Modified by Martin Peris Martorell (info@martinperis.com) in order to accept some configuration
+	parameters and store all the calibration data as xml files.
+
+*/
+
+#include "cv.h"
+#include "cxmisc.h"
+#include "highgui.h"
+#include "cvaux.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+//
+// Given a list of chessboard images, the number of corners (nx, ny)
+// on the chessboards, and a flag: useCalibrated for calibrated (0) or
+// uncalibrated (1: use cvStereoCalibrate(), 2: compute fundamental
+// matrix separately) stereo. Calibrate the cameras and display the
+// rectified results along with the computed disparity images.
+//
+static void
+StereoCalib(const char* imageList, int nx, int ny, int useUncalibrated, float _squareSize)
+{
+    int displayCorners = 1;
+    int showUndistorted = 1;
+    bool isVerticalStereo = false;//OpenCV can handle left-right
+                                      //or up-down camera arrangements
+    const int maxScale = 1;
+    const float squareSize = _squareSize; //Chessboard square size in cm
+    FILE* f = fopen(imageList, "rt");
+    int i, j, lr, nframes, n = nx*ny, N = 0;
+    vector<string> imageNames[2];
+    vector<CvPoint3D32f> objectPoints;
+    vector<CvPoint2D32f> points[2];
+    vector<int> npoints;
+    vector<uchar> active[2];
+    vector<CvPoint2D32f> temp(n);
+    CvSize imageSize = {0,0};
+    // ARRAY AND VECTOR STORAGE:
+    //double M1[3][3], M2[3][3], D1[8], D2[8];
+    double R[3][3], T[3], E[3][3], F[3][3];
+    double Q[4][4];
+    
+    Mat m1(3,3, CV_64F);
+    Mat m2(3,3, CV_64F);
+    Mat d1(1,8, CV_64F);
+    Mat d2(1,8, CV_64F);
+    
+    m1.at<double>(0,0) = 198.6194578177054f;
+    m1.at<double>(0,1) = 0;
+    m1.at<double>(0,2) = 187.5;
+    
+    m1.at<double>(1,0) = 0;
+    m1.at<double>(1,1) = 198.6194578177054;
+    m1.at<double>(1,2) = 119.5;
+    
+    m1.at<double>(2,0) = 0;
+    m1.at<double>(2,1) = 0;
+    m1.at<double>(2,2) = 1;
+    
+    d1.at<double>(0) = -0.04778439940911512;
+    d1.at<double>(1) = 0.07049708157561842;
+    d1.at<double>(2) = 0;
+    d1.at<double>(3) = 0;
+    d1.at<double>(4) = -0.002955310122296933;
+    d1.at<double>(5) = 0.2618861478373317;
+    d1.at<double>(6) = 0.03796295854262224;
+    d1.at<double>(7) = 0.008647825808491771;
+
+    m2.at<double>(0,0) = 196.4226596594262;
+    m2.at<double>(0,1) = 0;
+    m2.at<double>(0,2) = 187.5;
+    
+    m2.at<double>(1,0) = 0;
+    m2.at<double>(1,1) = 196.4226596594262;
+    m2.at<double>(1,2) = 119.5;
+    
+    m2.at<double>(2,0) = 0;
+    m2.at<double>(2,1) = 0;
+    m2.at<double>(2,2) = 1;
+    
+    
+    d2.at<double>(0) = 3.006377146047796;
+    d2.at<double>(1) = -0.590649654651801;
+    d2.at<double>(2) = 0;
+    d2.at<double>(3) = 0;
+    d2.at<double>(4) = 0.3184389523055426;
+    d2.at<double>(5) = 3.477236915494405;
+    d2.at<double>(6) = 0.02556491453960398;
+    d2.at<double>(7) = 0.3297662362259657;
+    
+    #if 0
+    CvMat _M1 = cvMat(3, 3, CV_64F, M1 );
+    CvMat _M2 = cvMat(3, 3, CV_64F, M2 );
+    CvMat _D1 = cvMat(1, 8, CV_64F, D1 );
+    CvMat _D2 = cvMat(1, 8, CV_64F, D2 );
+    #endif
+    
+    CvMat _M1 = m1;
+    CvMat _M2 = m2;
+    CvMat _D1 = d1;
+    CvMat _D2 = d2;
+    
+    CvMat _R = cvMat(3, 3, CV_64F, R );
+    CvMat _T = cvMat(3, 1, CV_64F, T );
+    CvMat _E = cvMat(3, 3, CV_64F, E );
+    CvMat _F = cvMat(3, 3, CV_64F, F );
+    CvMat _Q = cvMat(4,4, CV_64F, Q);
+    
+    
+cout << "m1 = " << m1 << endl;
+cout << "d1 = " << d1 << endl;
+cout << "m2 = " << m2 << endl;
+cout << "d2 = " << d2 << endl;
+
+    if( displayCorners )
+        cvNamedWindow( "corners", 1 );
+// READ IN THE LIST OF CHESSBOARDS:
+    if( !f )
+    {
+        fprintf(stderr, "can not open file %s\n", imageList );
+        return;
+    }
+    for(i=0;;i++)
+    {
+        char buf[1024];
+        int count = 0, result=0;
+        lr = i % 2;
+        vector<CvPoint2D32f>& pts = points[lr];
+        if( !fgets( buf, sizeof(buf)-3, f ))
+            break;
+        size_t len = strlen(buf);
+        while( len > 0 && isspace(buf[len-1]))
+            buf[--len] = '\0';
+        if( buf[0] == '#')
+            continue;
+        IplImage* img = cvLoadImage( buf, 0 );
+        if( !img )
+            break;
+        imageSize = cvGetSize(img);
+        imageNames[lr].push_back(buf);
+    //FIND CHESSBOARDS AND CORNERS THEREIN:
+        for( int s = 1; s <= maxScale; s++ )
+        {
+            IplImage* timg = img;
+            if( s > 1 )
+            {
+                timg = cvCreateImage(cvSize(img->width*s,img->height*s),
+                    img->depth, img->nChannels );
+                cvResize( img, timg, CV_INTER_CUBIC );
+            }
+            result = cvFindChessboardCorners( timg, cvSize(nx, ny),
+                &temp[0], &count,
+                CV_CALIB_CB_ADAPTIVE_THRESH |
+                CV_CALIB_CB_NORMALIZE_IMAGE);
+            if( timg != img )
+                cvReleaseImage( &timg );
+            if( result || s == maxScale )
+                for( j = 0; j < count; j++ )
+            {
+                temp[j].x /= s;
+                temp[j].y /= s;
+            }
+            if( result )
+                break;
+        }
+        if( displayCorners )
+        {
+            printf("%s\n", buf);
+            IplImage* cimg = cvCreateImage( imageSize, 8, 3 );
+            cvCvtColor( img, cimg, CV_GRAY2BGR );
+            cvDrawChessboardCorners( cimg, cvSize(nx, ny), &temp[0],
+                count, result );
+            cvShowImage( "corners", cimg );
+            cvReleaseImage( &cimg );
+            if( cvWaitKey(0) == 27 ) //Allow ESC to quit
+                exit(-1);
+        }
+        else
+            putchar('.');
+        N = pts.size();
+        pts.resize(N + n, cvPoint2D32f(0,0));
+        active[lr].push_back((uchar)result);
+    //assert( result != 0 );
+        if( result )
+        {
+         //Calibration will suffer without subpixel interpolation
+            cvFindCornerSubPix( img, &temp[0], count,
+                cvSize(11, 11), cvSize(-1,-1),
+                cvTermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS,
+                30, 0.01) );
+            copy( temp.begin(), temp.end(), pts.begin() + N );
+        }
+        cvReleaseImage( &img );
+    }
+    fclose(f);
+    printf("\n");
+// HARVEST CHESSBOARD 3D OBJECT POINT LIST:
+    nframes = active[0].size();//Number of good chessboads found
+    objectPoints.resize(nframes*n);
+    for( i = 0; i < ny; i++ )
+        for( j = 0; j < nx; j++ )
+        objectPoints[i*nx + j] = cvPoint3D32f(i*squareSize, j*squareSize, 0);
+    for( i = 1; i < nframes; i++ )
+        copy( objectPoints.begin(), objectPoints.begin() + n,
+        objectPoints.begin() + i*n );
+    npoints.resize(nframes,n);
+    N = nframes*n;
+    CvMat _objectPoints = cvMat(1, N, CV_32FC3, &objectPoints[0] );
+    CvMat _imagePoints1 = cvMat(1, N, CV_32FC2, &points[0][0] );
+    CvMat _imagePoints2 = cvMat(1, N, CV_32FC2, &points[1][0] );
+    CvMat _npoints = cvMat(1, npoints.size(), CV_32S, &npoints[0] );
+    //cvSetIdentity(&_M1);
+    //cvSetIdentity(&_M2);
+    //cvZero(&_D1);
+    //cvZero(&_D2);
+    
+    cvNamedWindow( "rect", 1 );
+    
+    
+// CALIBRATE THE STEREO CAMERAS
+    printf("Running stereo calibration ...\n");
+    fflush(stdout);
+    
+     CvScalar scal;
+    for(i=0;i<8;i++)
+    {
+        scal = cvGet1D(&_D1,i);
+        printf("val:%f \n",scal.val[0]);
+    }
+    
+    
+    cvStereoCalibrate( &_objectPoints, &_imagePoints1,
+        &_imagePoints2, &_npoints,
+        &_M1, &_D1, &_M2, &_D2,
+        imageSize, &_R, &_T, &_E, &_F,
+        cvTermCriteria(CV_TERMCRIT_ITER+
+        CV_TERMCRIT_EPS, 30, 1e-6),
+        CV_CALIB_RATIONAL_MODEL | CV_CALIB_USE_INTRINSIC_GUESS|
+            CV_CALIB_FIX_ASPECT_RATIO );
+    printf(" done\n");
+    
+     for(i=0;i<8;i++)
+    {
+        scal = cvGet1D(&_D1,i);
+        printf("val:%f \n",scal.val[0]);
+    }
+// CALIBRATION QUALITY CHECK
+// because the output fundamental matrix implicitly
+// includes all the output information,
+// we can check the quality of calibration using the
+// epipolar geometry constraint: m2^t*F*m1=0
+    vector<CvPoint3D32f> lines[2];
+    points[0].resize(N);
+    points[1].resize(N);
+    _imagePoints1 = cvMat(1, N, CV_32FC2, &points[0][0] );
+    _imagePoints2 = cvMat(1, N, CV_32FC2, &points[1][0] );
+    lines[0].resize(N);
+    lines[1].resize(N);
+    CvMat _L1 = cvMat(1, N, CV_32FC3, &lines[0][0]);
+    CvMat _L2 = cvMat(1, N, CV_32FC3, &lines[1][0]);
+//Always work in undistorted space
+    cvUndistortPoints( &_imagePoints1, &_imagePoints1,
+        &_M1, &_D1, 0, &_M1 );
+    cvUndistortPoints( &_imagePoints2, &_imagePoints2,
+        &_M2, &_D2, 0, &_M2 );
+    cvComputeCorrespondEpilines( &_imagePoints1, 1, &_F, &_L1 );
+    cvComputeCorrespondEpilines( &_imagePoints2, 2, &_F, &_L2 );
+    double avgErr = 0;
+    for( i = 0; i < N; i++ )
+    {
+        double err = fabs(points[0][i].x*lines[1][i].x +
+            points[0][i].y*lines[1][i].y + lines[1][i].z)
+            + fabs(points[1][i].x*lines[0][i].x +
+            points[1][i].y*lines[0][i].y + lines[0][i].z);
+        avgErr += err;
+    }
+    printf( "avg err = %g\n", avgErr/(nframes*n) );
+//COMPUTE AND DISPLAY RECTIFICATION
+    if( showUndistorted )
+    {
+        CvMat* mx1 = cvCreateMat( imageSize.height,
+            imageSize.width, CV_32F );
+        CvMat* my1 = cvCreateMat( imageSize.height,
+            imageSize.width, CV_32F );
+        CvMat* mx2 = cvCreateMat( imageSize.height,
+
+            imageSize.width, CV_32F );
+        CvMat* my2 = cvCreateMat( imageSize.height,
+            imageSize.width, CV_32F );
+        CvMat* img1r = cvCreateMat( imageSize.height,
+            imageSize.width, CV_8U );
+        CvMat* img2r = cvCreateMat( imageSize.height,
+            imageSize.width, CV_8U );
+        CvMat* disp = cvCreateMat( imageSize.height,
+            imageSize.width, CV_16S );
+        CvMat* vdisp = cvCreateMat( imageSize.height,
+            imageSize.width, CV_8U );
+        CvMat* pair;
+        double R1[3][3], R2[3][3], P1[3][4], P2[3][4];
+        CvMat _R1 = cvMat(3, 3, CV_64F, R1);
+        CvMat _R2 = cvMat(3, 3, CV_64F, R2);
+// IF BY CALIBRATED (BOUGUET'S METHOD)
+        if( useUncalibrated == 0 )
+        {
+            CvMat _P1 = cvMat(3, 4, CV_64F, P1);
+            CvMat _P2 = cvMat(3, 4, CV_64F, P2);
+            cvStereoRectify( &_M1, &_M2, &_D1, &_D2, imageSize,
+                &_R, &_T,
+                &_R1, &_R2, &_P1, &_P2, &_Q,
+                0/*CV_CALIB_ZERO_DISPARITY*/ );
+            isVerticalStereo = fabs(P2[1][3]) > fabs(P2[0][3]);
+    //Precompute maps for cvRemap()
+            cvInitUndistortRectifyMap(&_M1,&_D1,&_R1,&_P1,mx1,my1);
+            cvInitUndistortRectifyMap(&_M2,&_D2,&_R2,&_P2,mx2,my2);
+            
+    //Save parameters
+            cvSave("M1.xml",&_M1);
+            cvSave("D1.xml",&_D1);
+            cvSave("R1.xml",&_R1);
+            cvSave("P1.xml",&_P1);
+            cvSave("M2.xml",&_M2);
+            cvSave("D2.xml",&_D2);
+            cvSave("R2.xml",&_R2);
+            cvSave("P2.xml",&_P2);
+            cvSave("Q.xml",&_Q);
+            cvSave("mx1.xml",mx1);
+            cvSave("my1.xml",my1);
+            cvSave("mx2.xml",mx2);
+            cvSave("my2.xml",my2);
+
+        }
+//OR ELSE HARTLEY'S METHOD
+        else if( useUncalibrated == 1 || useUncalibrated == 2 )
+     // use intrinsic parameters of each camera, but
+     // compute the rectification transformation directly
+     // from the fundamental matrix
+        {
+            double H1[3][3], H2[3][3], iM[3][3];
+            CvMat _H1 = cvMat(3, 3, CV_64F, H1);
+            CvMat _H2 = cvMat(3, 3, CV_64F, H2);
+            CvMat _iM = cvMat(3, 3, CV_64F, iM);
+    //Just to show you could have independently used F
+            if( useUncalibrated == 2 )
+                cvFindFundamentalMat( &_imagePoints1,
+                &_imagePoints2, &_F);
+            cvStereoRectifyUncalibrated( &_imagePoints1,
+                &_imagePoints2, &_F,
+                imageSize,
+                &_H1, &_H2, 3);
+            cvInvert(&_M1, &_iM);
+            cvMatMul(&_H1, &_M1, &_R1);
+            cvMatMul(&_iM, &_R1, &_R1);
+            cvInvert(&_M2, &_iM);
+            cvMatMul(&_H2, &_M2, &_R2);
+            cvMatMul(&_iM, &_R2, &_R2);
+    //Precompute map for cvRemap()
+            cvInitUndistortRectifyMap(&_M1,&_D1,&_R1,&_M1,mx1,my1);
+
+            cvInitUndistortRectifyMap(&_M2,&_D1,&_R2,&_M2,mx2,my2);
+        }
+        else
+            assert(0);
+        cvNamedWindow( "rectified", 1 );
+// RECTIFY THE IMAGES AND FIND DISPARITY MAPS
+        if( !isVerticalStereo )
+            pair = cvCreateMat( imageSize.height, imageSize.width*2,
+            CV_8UC3 );
+        else
+            pair = cvCreateMat( imageSize.height*2, imageSize.width,
+            CV_8UC3 );
+//Setup for finding stereo corrrespondences
+        CvStereoBMState *BMState = cvCreateStereoBMState();
+        assert(BMState != 0);
+        BMState->preFilterSize=41;
+        BMState->preFilterCap=31;
+        BMState->SADWindowSize=41;
+        BMState->minDisparity=-64;
+        BMState->numberOfDisparities=128;
+        BMState->textureThreshold=10;
+        BMState->uniquenessRatio=15;
+        for( i = 0; i < nframes; i++ )
+        {
+            IplImage* img1=cvLoadImage(imageNames[0][i].c_str(),0);
+            IplImage* img2=cvLoadImage(imageNames[1][i].c_str(),0);
+            if( img1 && img2 )
+            {
+                CvMat part;
+                cvRemap( img1, img1r, mx1, my1 );
+                cvRemap( img2, img2r, mx2, my2 );
+                if( !isVerticalStereo || useUncalibrated != 0 )
+                {
+              // When the stereo camera is oriented vertically,
+              // useUncalibrated==0 does not transpose the
+              // image, so the epipolar lines in the rectified
+              // images are vertical. Stereo correspondence
+              // function does not support such a case.
+                    cvFindStereoCorrespondenceBM( img1r, img2r, disp,
+                        BMState);
+                    cvNormalize( disp, vdisp, 0, 256, CV_MINMAX );
+                    cvNamedWindow( "disparity" );
+                    cvShowImage( "disparity", vdisp );
+                }
+                if( !isVerticalStereo )
+                {
+                    cvGetCols( pair, &part, 0, imageSize.width );
+                    cvCvtColor( img1r, &part, CV_GRAY2BGR );
+                    cvGetCols( pair, &part, imageSize.width,
+                        imageSize.width*2 );
+                    cvCvtColor( img2r, &part, CV_GRAY2BGR );
+                    for( j = 0; j < imageSize.height; j += 16 )
+                        cvLine( pair, cvPoint(0,j),
+                        cvPoint(imageSize.width*2,j),
+                        CV_RGB(0,255,0));
+                }
+                else
+                {
+                    cvGetRows( pair, &part, 0, imageSize.height );
+                    cvCvtColor( img1r, &part, CV_GRAY2BGR );
+                    cvGetRows( pair, &part, imageSize.height,
+                        imageSize.height*2 );
+                    cvCvtColor( img2r, &part, CV_GRAY2BGR );
+                    for( j = 0; j < imageSize.width; j += 16 )
+                        cvLine( pair, cvPoint(j,0),
+                        cvPoint(j,imageSize.height*2),
+                        CV_RGB(0,255,0));
+                }
+                cvShowImage( "rectified", pair );
+                if( cvWaitKey() == 27 )
+                    break;
+            }
+            cvReleaseImage( &img1 );
+            cvReleaseImage( &img2 );
+        }
+        cvReleaseStereoBMState(&BMState);
+        cvReleaseMat( &mx1 );
+        cvReleaseMat( &my1 );
+        cvReleaseMat( &mx2 );
+        cvReleaseMat( &my2 );
+        cvReleaseMat( &img1r );
+        cvReleaseMat( &img2r );
+        cvReleaseMat( &disp );
+    }
+}
+int main(int argc, char *argv[])
+{
+    int nx, ny;
+    float squareSize;
+    int fail = 0;
+    //Check command line
+    if (argc != 5)
+    {
+        fprintf(stderr,"USAGE: %s imageList nx ny squareSize\n",argv[0]);
+        fprintf(stderr,"\t imageList : Filename of the image list (string). Example : list.txt\n");
+        fprintf(stderr,"\t nx : Number of horizontal squares (int > 0). Example : 9\n");
+        fprintf(stderr,"\t ny : Number of vertical squares (int > 0). Example : 6\n");
+        fprintf(stderr,"\t squareSize : Size of a square (float > 0). Example : 2.5\n");
+        return 1;
+    } 
+
+		nx = atoi(argv[2]);
+    ny = atoi(argv[3]);
+    squareSize = (float)atof(argv[4]);
+
+    if (nx <= 0)
+    {
+        fail = 1;
+        fprintf(stderr, "ERROR: nx value can not be <= 0\n");
+    }
+    if (ny <= 0)
+    {
+        fail = 1;
+        fprintf(stderr, "ERROR: ny value can not be <= 0\n");
+    }   
+    if (squareSize <= 0.0)
+    {
+        fail = 1;
+        fprintf(stderr, "ERROR: squareSize value can not be <= 0\n");
+    }   
+
+    if(fail != 0) return 1;
+
+    StereoCalib(argv[1], nx, ny, 0, squareSize);
+    return 0;
+}
