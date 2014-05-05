3c600ce37930622200f702d29806e02e97a3bd2d
Andy Barry <abarry@gmail.com>
2014-05-05 18:14:34 -0400

    Adds octomap functions from stereo-imu-obstacles as its own class, which should be really nice to link against.

---
+++ estimators/StereoOctomap/StereoOctomap.cpp
@@ -0,0 +1,116 @@
+#include "StereoOctomap.hpp"
+
+
+StereoOctomap::StereoOctomap(BotFrames *bot_frames) {
+    bot_frames_ = bot_frames;
+    
+    current_octree_ = new OcTree(0.1);
+    building_octree_ = new OcTree(0.1);
+    
+    current_octree_timestamp_ = -1;
+    building_octree_timestamp_ = -1;
+    
+}
+
+
+void StereoOctomap::ProcessStereoMessage(const lcmt_stereo *msg) {
+    
+    // get transform from global to body frame
+    BotTrans to_open_cv, body_to_local;
+    bot_frames_get_trans(botFrames, "opencvFrame", "local", &to_open_cv);
+    bot_frames_get_trans(botFrames, "body", "local", &body_to_local);
+    
+    
+    // insert the points into the octree
+    InsertPointsIntoOctree(msg, &to_open_cv, &body_to_local);
+    
+    // zap the old points from the tree
+    RemoveOldPoints(msg->timestamp);
+    
+}
+
+void StereoOctomap::InsertPointsIntoOctree(const lcmt_stereo *msg, BotTrans *to_open_cv, BotTrans *body_to_local) {
+    // get the new origin
+    double origin[3];
+    origin[0] = 0;
+    origin[1] = 0;
+    origin[2] = 0;
+    
+    double plane_origin[3];
+    bot_trans_apply_vec(body_to_local, origin, plane_origin);
+    octomath::Vector3 vec_plane_origin(plane_origin[0], plane_origin[1], plane_origin[2]);
+    
+    
+    // now apply this matrix to each point
+    for (int i = 0; i<msg->number_of_points; i++)
+    {
+        Vector3d this_point;
+        this_point << msg->z[i], msg->x[i], msg->y[i];
+        double this_point_d[3];
+        double trans_point[3];
+        this_point_d[0] = msg->x[i]/10;
+        this_point_d[1] = msg->y[i]/10;
+        
+        // opencv sends the z coordinate reversed from what we expect and is also in cm
+        this_point_d[2] = -msg->z[i]/10;
+        
+        // add the position vector
+        bot_trans_apply_vec(to_open_cv, this_point_d, trans_point);
+        
+        octomath::Vector3 vec_new_point(trans_point[0], trans_point[1], trans_point[2]);
+        
+        // add this point to the octree
+        currentOctree->insertRay(vec_plane_origin, vec_new_point);
+        buildingOctree->insertRay(vec_plane_origin, vec_new_point);
+        
+    }
+}
+
+
+void StereoOctomap::RemoveOldPoints(int64_t last_msg_time) {
+    // check timestamps
+    if (current_octree_timestamp_ < 0 || current_octree_timestamp_ > last_msg_time) // the second case can happen if you're replaying a log and jump back
+    {
+         current_octree_timestamp_ = last_msg_time;
+         building_octree_timestamp_ = last_msg_time + OCTREE_LIFE/2; // half a life in the future
+    } else {
+        if (current_octree_timestamp_ + OCTREE_LIFE < last_msg_time)
+        {
+            // swap out trees since this one has expired
+            delete current_octree_;
+            
+            current_octree_timestamp_ = building_octree_timestamp_;
+            building_octree_timestamp_ = last_msg_time;
+            
+            current_octree_ = building_octree_;
+            building_octree_ = new OcTree(0.1);
+            
+            cout << endl << "swapping octrees" << endl;
+        }
+    }
+    //currentOctree->degradeOutdatedNodes(2);
+}
+
+
+void StereoOctomap::PublishOctomap(lcm_t *lcm) {
+
+    // publish octomap to LCM
+    
+    octomap_raw_t oc_msg;
+    oc_msg.utime = getTimestampNow();
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            oc_msg.transform[i][j] = 0;
+        }
+        oc_msg.transform[i][i] = 1;
+    }
+
+    std::stringstream datastream;
+    currentOctree->writeBinaryConst(datastream);
+    std::string datastring = datastream.str();
+    oc_msg.data = (uint8_t *) datastring.c_str();
+    oc_msg.length = datastring.size();
+
+    octomap_raw_t_publish(lcm, "OCTOMAP", &oc_msg);
+}
---
+++ estimators/StereoOctomap/StereoOctomap.hpp
@@ -0,0 +1,58 @@
+#ifndef STEREO_OCTOMAP_H_
+#define STEREO_OCTOMAP_H_
+
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+
+#include <Eigen/Dense>
+
+#include <GL/gl.h>
+#include <bot_lcmgl_client/lcmgl.h>
+#include <bot_core/rotations.h>
+#include <bot_frames/bot_frames.h>
+#include <bot_param/param_client.h>
+#include <lcmtypes/octomap_raw_t.h>
+#include <octomap/OcTree.h>
+#include "../../LCM/lcmt_stereo.h"
+
+#define OCTREE_LIFE 2000 // in msec
+
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
+
+using namespace std;
+using namespace octomap;
+
+
+
+class StereoOctomap {
+    
+    public:
+        
+        StereoOctomap(BotFrames *bot_frames);
+        
+        void ProcessStereoMessage(const lcmt_stereo *msg);
+        
+        void PublishOctomap(lcm_t *lcm) {
+);
+        
+        
+    private:
+    
+        void InsertPointsIntoOctree(const lcmt_stereo *msg, BotTrans *to_open_cv, BotTrans *body_to_local);
+        void RemoveOldPoints(int64_t last_msg_time)
+    
+        OcTree *current_octree_;
+        OcTree *building_octree_;
+        
+        int64_t current_octree_timestamp_, building_octree_timestamp_;
+        
+        BotFrames *bot_frames_;
+        
+        
+
+};
+
+#endif
