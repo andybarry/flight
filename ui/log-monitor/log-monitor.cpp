6ad94c16a83bf1d3e66366ef4d4711f1e76dd071
Andy Barry <abarry@gmail.com>
2015-02-23 23:19:18 -0500

    Updates log monitor to use hostnames.  Updates config files for cam1 computer configuration.

--- ui/log-monitor/log-monitor.cpp
+++ ui/log-monitor/log-monitor.cpp
@@ -18,176 +18,151 @@ using namespace std;
 #include <time.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include <dirent.h>
-
 
 #include "../../LCM/lcmt_log_size.h"
 
-#include <bot_core/bot_core.h>
-#include <bot_param/param_client.h>
-#include <GL/gl.h>
-#include <bot_lcmgl_client/lcmgl.h>
+#include "../../externals/ConciseArgs.hpp"
 
-#include <bot_core/rotations.h>
-#include <bot_frames/bot_frames.h>
-   
-   
+#include "../../utils/utils/RealtimeUtils.hpp"
+
 
 lcm_t * lcm;
 
-char *channelLogSize = NULL;
-string logDir;
+string log_dir = "/home/odroid";
+string log_info_channel_str = "log-info-hostname";
 
 
-static void usage(void)
-{
-        fprintf(stderr, "usage: log-monitor log-size-channel full-directory-path\n");
-        fprintf(stderr, "    log-size-channel: LCM channel name with publish log size messages on\n");
-        fprintf(stderr, "    full directory path: directory logfiles are\n");
-        fprintf(stderr, "  example:\n");
-        fprintf(stderr, "    ./log-monitor log_size /home/linaro/\n");
-}
-
-
-
-void sighandler(int dum)
-{
+void sighandler(int dum) {
     printf("\nClosing... ");
 
     lcm_destroy (lcm);
 
     printf("done.\n");
-    
+
     exit(0);
 }
 
-int64_t getTimestampNow()
-{
-    struct timeval thisTime;
-    gettimeofday(&thisTime, NULL);
-    return (thisTime.tv_sec * 1000000.0) + (float)thisTime.tv_usec + 0.5;
-}
-
-long GetFileSize(std::string filename)
-{
+long GetFileSize(std::string filename) {
     struct stat stat_buf;
     int rc = stat(filename.c_str(), &stat_buf);
     return rc == 0 ? stat_buf.st_size : -1;
 }
 
 
-int main(int argc,char** argv)
-{
-    if (argc!=3) {
-        usage();
-        exit(0);
-    }
+int main(int argc,char** argv) {
 
-    channelLogSize = argv[1];
-    logDir = string(argv[2]);
-    
-    if (!logDir.empty())
-    {
-        char lastChar = *logDir.rbegin();
-        if (lastChar != '/')
-        {
-            logDir = logDir + "/";
+    // use this computers hostname by default
+    char hostname[100];
+    size_t hostname_len = 100;
+
+    gethostname(hostname, hostname_len);
+    log_info_channel_str = "log-info-" + string(hostname);
+
+    ConciseArgs parser(argc, argv);
+    parser.add(log_info_channel_str, "c", "log-info-channel",
+        "LCM channel for publishing log info.");
+    parser.add(log_dir, "d", "log-directory",
+        "Directory containing log files.", true);
+    parser.parse();
+
+    if (!log_dir.empty()) {
+        char last_char = *log_dir.rbegin();
+        if (last_char != '/') {
+            log_dir = log_dir + "/";
         }
     } else {
         fprintf(stderr, "directory can't be empty.  Try \".\" if you want this directory.\n");
         return 1;
     }
-    
+
 
     lcm = lcm_create ("udpm://239.255.76.67:7667?ttl=1");
-    if (!lcm)
-    {
+    if (!lcm) {
         fprintf(stderr, "lcm_create failed.  Quitting.\n");
         return 1;
     }
 
     signal(SIGINT,sighandler);
-    
-    
-    
-    printf("Publishing: Log size:\n\t%s\n\n", channelLogSize);
 
-    while (true)
-    {
+
+
+    printf("Publishing:\n\tLog size: %s\n\n", log_info_channel_str.c_str());
+
+    while (true) {
         // find the logfile for today
         // formatted something like "lcmlog-2013-07-18.00"
-        
+
         // get the date
         time_t rawtime;
         struct tm * timeinfo;
-        char dateString [80];
+        char date_string[80];
 
         time ( &rawtime );
         timeinfo = localtime ( &rawtime );
 
-        strftime (dateString,80,"%Y-%m-%d",timeinfo);
-        
-        string lcmPrefix = "lcmlog-" + string(dateString) + ".";
-        
-        int largestLog = -1;
-        string theLog;
-        
+        strftime (date_string, 80, "%Y-%m-%d", timeinfo);
+
+        string lcm_prefix = "lcmlog-" + string(date_string) + ".";
+
+        int largest_log = -1;
+        int log_size = -1;
+        string the_log;
+
         DIR *dir;
         struct dirent *ent;
-        if ((dir = opendir (logDir.c_str())) != NULL) {
-          /* print all the files and directories within directory */
-          while ((ent = readdir (dir)) != NULL) {
-            //printf ("%s\n", ent->d_name);
-            // see if this name starts with lcmlog-
-            
-            string thisName = ent->d_name;
-            
-            if (!thisName.compare(0, lcmPrefix.size(), lcmPrefix))
-            {
-                // get the number of this log
-                string numString = thisName.substr(lcmPrefix.size());
-                int logNum = atoi(numString.c_str());
-                
-                if (logNum > largestLog)
-                {
-                    largestLog = logNum;
-                    theLog = thisName;
+        if ((dir = opendir (log_dir.c_str())) != NULL) {
+            /* print all the files and directories within directory */
+            while ((ent = readdir (dir)) != NULL) {
+                //printf ("%s\n", ent->d_name);
+                // see if this name starts with lcmlog-
+
+                string this_name = ent->d_name;
+
+                if (!this_name.compare(0, lcm_prefix.size(), lcm_prefix)) {
+                    // get the number of this log
+                    string num_string = this_name.substr(lcm_prefix.size());
+                    int log_num = atoi(num_string.c_str());
+
+                    if (log_num > largest_log) {
+                        largest_log = log_num;
+                        the_log = this_name;
+                    }
                 }
+
             }
-            
-          }
-          closedir (dir);
+            closedir (dir);
         } else {
           /* could not open directory */
           perror ("");
           return EXIT_FAILURE;
         }
-        
-        if (largestLog < 0)
-        {
+
+        if (largest_log < 0) {
             // didn't find a log
             //cout << "Failed to find any logs" << endl;
+
+            log_size = -1;
+
         } else {
             // found a log, get the filesize
-            
-            
+
+
             //cout << "Filesize of log " << theLog << " is " << GetFileSize(string(logDir) + theLog) << endl;
-            
-            // publish to LCM
-            lcmt_log_size msg;
 
-            msg.timestamp = getTimestampNow();
+            log_size = GetFileSize(string(log_dir) + the_log);
 
-            msg.log_number = largestLog;
-            msg.log_size = GetFileSize(string(logDir) + theLog);
-
-            lcmt_log_size_publish (lcm, channelLogSize, &msg);
-            
         }
-        
+
+        // publish to LCM
+        lcmt_log_size msg;
+
+        msg.timestamp = GetTimestampNow();
+
+        msg.log_number = largest_log;
+        msg.log_size = log_size;
+
+        lcmt_log_size_publish (lcm, log_info_channel_str.c_str(), &msg);
+
         sleep(1);
     }
 
