5484eeef661527b132d149ac215605ea87280c50
Andy Barry <abarry@gmail.com>
2015-04-27 22:56:49 -0400

    Adds odroid-u3 fan control to cpu-monitor.

--- drivers/cpu-monitor/cpu-monitor.cpp
+++ drivers/cpu-monitor/cpu-monitor.cpp
@@ -37,20 +37,55 @@ string cpu_freq_file =  "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq";
 
 string cpu_temp_file = "/sys/class/thermal/thermal_zone0/temp";
 
+string fan_auto_manual_file = "/sys/devices/platform/odroidu2-fan/fan_mode";
+
+string fan_pwm_file = "/sys/devices/platform/odroidu2-fan/pwm_duty";
+
 
 lcm_t * lcm;
+
+bool disable_fan_control = false;
 
 string cpu_info_channel_str = "cpu-info-hostname";
 
 
-void sighandler(int dum)
-{
+void sighandler(int dum) {
+    printf("\nRestoring automatic fan control...");
+    ofstream auto_man_file;
+    auto_man_file.open(fan_auto_manual_file);
+    auto_man_file << "auto";
+    auto_man_file.close();
+
     printf("\nClosing... ");
     lcm_destroy (lcm);
 
     printf("done.\n");
 
+
     exit(0);
+}
+
+void SetFanSpeed(float cpu_temp, int fan_pwm) {
+
+    int new_pwm;
+
+    if (cpu_temp > 69) {
+        new_pwm = 255;
+    } else if (cpu_temp > 62) {
+        new_pwm = 128;
+    } else {
+        new_pwm = 0;
+    }
+
+    if (new_pwm != fan_pwm) {
+        // set the new fan speed
+
+        ofstream pwm_file;
+        pwm_file.open(fan_pwm_file);
+        pwm_file << std::to_string(new_pwm);
+        pwm_file.close();
+
+    }
 }
 
 void PublishCpuInfo() {
@@ -73,9 +108,31 @@ void PublishCpuInfo() {
     lcmt_cpu_info msg;
     msg.cpu_freq = cpu_freq;
 
+    // get CPU temp
     float cpu_temp = stof(cpu_temp_string);
     cpu_temp /= 1000;
     msg.cpu_temp = cpu_temp;
+
+
+    // get fan speed
+    if (!disable_fan_control) {
+        ifstream fan_file_stream(fan_pwm_file);
+        string fan_string;
+        getline(fan_file_stream, fan_string);
+        fan_file_stream.close();
+
+        fan_string = fan_string.substr(22, string::npos);
+        std::size_t first_space = fan_string.find_first_of(" ");
+
+        fan_string = fan_string.substr(0, first_space);
+
+        int fan_pwm = stoi(fan_string);
+        msg.fan_pwm = fan_pwm;
+
+        SetFanSpeed(cpu_temp, fan_pwm);
+    } else {
+        msg.fan_pwm = -1;
+    }
 
     msg.timestamp = GetTimestampNow();
 
@@ -100,6 +157,7 @@ int main(int argc,char** argv) {
         "File to read containing CPU frequency.");
     parser.add(cpu_temp_file, "t", "cpu-temp-file",
         "File to read containing CPU temperature.");
+    parser.add(disable_fan_control, "n", "disable-fan-control", "Pass to disable ODROID-U3 fan control.");
     parser.parse();
 
     lcm = lcm_create ("udpm://239.255.76.67:7667?ttl=1");
@@ -109,6 +167,15 @@ int main(int argc,char** argv) {
     }
 
     signal(SIGINT,sighandler);
+
+    if (!disable_fan_control) {
+        printf("Taking fan control...\n");
+
+        ofstream auto_man_file;
+        auto_man_file.open(fan_auto_manual_file);
+        auto_man_file << "manual";
+        auto_man_file.close();
+    }
 
     printf("Publishing:\n\tCPU Info: %s\n", cpu_info_channel_str.c_str());
 
