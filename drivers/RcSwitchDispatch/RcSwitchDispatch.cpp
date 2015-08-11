22b1e1dbabc95aed787cc6744b444fe401ba5e12
Andy Barry <abarry@gmail.com>
2015-08-11 19:37:25 -0400

    Work on glue code for RC controller sending trajectories and stereo messages.  Getting close.

--- drivers/RcSwitchDispatch/RcSwitchDispatch.cpp
+++ drivers/RcSwitchDispatch/RcSwitchDispatch.cpp
@@ -1,18 +1,142 @@
 #include "RcSwitchDispatch.hpp"
 #include "../../externals/ConciseArgs.hpp"
 
-RcSwitchDispatch::RcSwitchDispatch(std::string rc_trajectory_commands_channel, std::string stereo_channel, int stable_traj_num) {
+RcSwitchDispatch::RcSwitchDispatch(lcm::LCM *lcm, std::string rc_trajectory_commands_channel, std::string stereo_channel) {
+    lcm_ = lcm;
     rc_trajectory_commands_channel_ = rc_trajectory_commands_channel;
     stereo_channel_ = stereo_channel;
+
+    param_ = bot_param_new_from_server(lcm_->getUnderlyingLCM(), 0);
+    bot_frames_ = bot_frames_new(lcm_->getUnderlyingLCM(), param_);
+
+    if (param_ == nullptr) {
+        std::cerr << "ERROR: failed to get a parameter server." << std::endl;
+        exit(1);
+    }
+
+    stable_traj_num_ = bot_param_get_int_or_fail(param_, "tvlqr_controller.stable_controller");
+    climb_traj_num_ = bot_param_get_int_or_fail(param_, "tvlqr_controller.climb_trajectroy");
+
+    int num_switch_positions = bot_param_get_int_or_fail(param_, "rc_switch_action.number_of_switch_positions");
+    num_trajs_ = bot_param_get_int_or_fail(param_, "rc_switch_action.number_of_trajectories");
+    num_stereo_actions_ = bot_param_get_int_or_fail(param_, "rc_switch_action.number_of_stereo_actions");
+    int num_unused = bot_param_get_int_or_fail(param_, "rc_switch_action.number_of_unused_slots");
+
+    if (num_trajs_ + num_stereo_actions_ + num_unused != num_switch_positions) {
+        std::cerr << "ERROR: num_trajs (" << num_trajs_ << ") + num_stereo (" << num_stereo_actions_ << ") + num_unused (" << num_unused << ") != num_switch_positions (" << num_switch_positions << ")" << std::endl;
+        exit(1);
+    }
+
+    bot_param_get_int_array_or_fail(param_, "rc_switch_action.trajectories", trajectory_mapping_, num_trajs_);
+    bot_param_get_int_array_or_fail(param_, "rc_switch_action.stereo_actions", stereo_mapping_, num_stereo_actions_);
+
+
+
 }
 
-void RcSwitchDispatch::ProcessRcTrajectoryMsg(const lcm::ReceiveBuffer *rbus, const std::string &chan, const lcmt::rc_switch_action *msg) {
+void RcSwitchDispatch::ProcessRcMsg(const lcm::ReceiveBuffer *rbus, const std::string &chan, const lcmt::rc_switch_action *msg) {
     // new message has come in from the RC controller -- figure out what to do
 
     if (msg->action < 0) {
         // stabilization mode
+        SendTrajectoryRequest(stable_traj_num_);
 
+    } else {
+        // not stabilization mode could be:
+        //      - run a different trajectory
+        //      - send stereo messages
+
+        if (msg->action < num_trajs_) {
+            SendTrajectoryRequest(trajectory_mapping_[msg->action]);
+        } else if (msg->action < num_trajs_ + num_stereo_actions_) {
+            SendStereoMsg(stereo_mapping_[msg->action]);
+        } else {
+            std::cerr << "WARNING: unused action requested: " << msg->action << std::endl;
+        }
     }
+}
+
+void RcSwitchDispatch::SendTrajectoryRequest(int traj_num) const {
+    lcmt::tvlqr_controller_action trajectory_msg;
+    trajectory_msg.timestamp = GetTimestampNow();
+
+    trajectory_msg.trajectory_number = traj_num;
+
+    lcm_->publish(rc_trajectory_commands_channel_, &trajectory_msg);
+}
+
+void RcSwitchDispatch::SendStereoMsg(int stereo_msg_num) const {
+    std::vector<float> x, y, z;
+
+    bool flag = true;
+
+    if (stereo_msg_num == 0) {
+        x = x_points0_;
+        y = y_points0_;
+        z = z_points0_;
+    } else {
+        flag = false;
+        std::cerr << "WARNING: unknown stereo message request: " << stereo_msg_num << std::endl;
+    }
+
+    if (flag == true) {
+        SendStereoManyPoints(x, y, z);
+    }
+}
+
+void RcSwitchDispatch::GlobalToCameraFrame(double point_in[], double point_out[]) const {
+    // figure out what this point (which is currently expressed in global coordinates
+    // will be in local opencv coordinates
+
+    BotTrans global_to_camera_trans;
+
+    // we must update this every time because the transforms could change as the aircraft
+    // moves
+    bot_frames_get_trans(bot_frames_, "local", "opencvFrame", &global_to_camera_trans);
+
+    bot_trans_apply_vec(&global_to_camera_trans, point_in, point_out);
+}
+
+void RcSwitchDispatch::SendStereoManyPoints(std::vector<float> x_in, std::vector<float> y_in, std::vector<float> z_in) const {
+    lcmt::stereo msg;
+
+    msg.timestamp = GetTimestampNow();
+
+    std::vector<float> x, y, z;
+    std::vector<unsigned char> grey;
+
+    for (int i = 0; i < (int)x_in.size(); i++) {
+
+        double this_point[3];
+
+        this_point[0] = x_in[i];
+        this_point[1] = y_in[i];
+        this_point[2] = z_in[i];
+
+        double point_transformed[3];
+        GlobalToCameraFrame(this_point, point_transformed);
+
+        ////std::cout << "Point: (" << point_transformed[0] << ", " << point_transformed[1] << ", " << point_transformed[2] << ")" << std::endl;
+
+        x.push_back(point_transformed[0]);
+        y.push_back(point_transformed[1]);
+        z.push_back(point_transformed[2]);
+
+        grey.push_back(0);
+    }
+
+    msg.x = x;
+    msg.y = y;
+    msg.z = z;
+
+    msg.grey = grey;
+
+    msg.number_of_points = x.size();
+    msg.video_number = 0;
+    msg.frame_number = 0;
+
+    lcm_->publish("stereo", &msg);
+
 }
 
 
@@ -29,7 +153,7 @@ int main(int argc,char** argv) {
     ConciseArgs parser(argc, argv);
     parser.add(ttl_one, "t", "ttl-one", "Pass to set LCM TTL=1");
     parser.add(stereo_channel, "s", "stereo-channel", "LCM channel to listen to stereo messages on.");
-    parser.add(rc_trajectory_commands_channel, "t", "rc-trajectory-commands-channel", "LCM channel to sned RC trajectory commands on.");
+    parser.add(rc_trajectory_commands_channel, "j", "rc-trajectory-commands-channel", "LCM channel to sned RC trajectory commands on.");
     parser.add(rc_action_channel, "r", "rc-action-channel", "LCM channel to listen for RC switch actions on.");
 
     parser.parse();
@@ -49,11 +173,12 @@ int main(int argc,char** argv) {
         return 1;
     }
 
-    RcSwitchDispatch rc_dispatch(rc_trajectory_commands_channel, stereo_channel);
+    RcSwitchDispatch rc_dispatch(&lcm, rc_trajectory_commands_channel, stereo_channel);
 
     // subscribe to LCM channels
-    lcm.subscribe(rc_action_channel, &RcSwitchDispatch::ProcessRcTrajectoryMsg, &rc_dispatch);
+    lcm.subscribe(rc_action_channel, &RcSwitchDispatch::ProcessRcMsg, &rc_dispatch);
 
+    printf("Recieving LCM:\n\tRC Switch Actions: %s\nSending LCM:\n\tRC Trajectory Requests: %s\n\tStereo Messages: %s\n", rc_action_channel.c_str(), rc_trajectory_commands_channel.c_str(), stereo_channel.c_str());
 
     while (0 == lcm.handle());
 
--- drivers/RcSwitchDispatch/RcSwitchDispatch.hpp
+++ drivers/RcSwitchDispatch/RcSwitchDispatch.hpp
@@ -12,20 +12,44 @@
 #include <lcm/lcm-cpp.hpp>
 #include "../../LCM/lcmt/tvlqr_controller_action.hpp"
 #include "../../LCM/lcmt/rc_switch_action.hpp"
+#include "../../LCM/lcmt/stereo.hpp"
 
+#include "../../utils/utils/RealtimeUtils.hpp"
 #include <bot_param/param_client.h>
+#include <vector>
+
+#define MAX_PARAM_SIZE 100
 
 class RcSwitchDispatch {
 
     public:
-        RcSwitchDispatch(std::string rc_trajectory_commands_channel, std::string stereo_channel, int stable_traj_num);
+        RcSwitchDispatch(lcm::LCM *lcm, std::string rc_trajectory_commands_channel, std::string stereo_channel);
 
-        void ProcessRcTrajectoryMsg(const lcm::ReceiveBuffer *rbus, const std::string &chan, const lcmt::rc_switch_action *msg);
+        void ProcessRcMsg(const lcm::ReceiveBuffer *rbus, const std::string &chan, const lcmt::rc_switch_action *msg);
 
     private:
+        void SendTrajectoryRequest(int traj_num) const;
+        void SendStereoMsg(int stereo_msg_num) const;
+        void GlobalToCameraFrame(double point_in[], double point_out[]) const;
+        void SendStereoManyPoints(std::vector<float> x_in, std::vector<float> y_in, std::vector<float> z_in) const;
+
         std::string stereo_channel_, rc_trajectory_commands_channel_;
 
-        int stable_traj_num_;
+        int stable_traj_num_, climb_traj_num_;
+        int num_trajs_, num_stereo_actions_;
+
+        int trajectory_mapping_[MAX_PARAM_SIZE];
+        int stereo_mapping_[MAX_PARAM_SIZE];
+
+        lcm::LCM *lcm_;
+
+        BotParam *param_;
+        BotFrames *bot_frames_;
+
+        std::vector<float> x_points0_ = {5};
+        std::vector<float> y_points0_ = {0};
+        std::vector<float> z_points0_ = {0};
+
 
 };
 
