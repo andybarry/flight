21628a6d2cec3714e4a6c64550bbd923fc982ad4
Andy Barry <abarry@gmail.com>
2016-01-01 19:07:20 -0500

    Removes .m files which have been moved to the simflight repo.

--- scripts/logs/ServoPercent.m
+++
@@ -1,3 +0,0 @@
-function out = ServoPercent(in)
-    out = in/8 - 1100/8;
-end
--- scripts/logs/TrimAirspeed.m
+++
@@ -1,8 +0,0 @@
-function airspeed = TrimAirspeed(start_time, end_time, airspeed_in)
-
-  airspeed.utime = TrimTimes(start_time, end_time, airspeed_in.logtime, airspeed_in.utime);
-  airspeed.airspeed = TrimTimes(start_time, end_time, airspeed_in.logtime, airspeed_in.airspeed);
-  
-  airspeed.logtime = TrimTimes(start_time, end_time, airspeed_in.logtime, airspeed_in.logtime);
-
-end
